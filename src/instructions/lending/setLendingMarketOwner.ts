48dee8f55a29bcf53dd61f4c42cd802cf48e4c53
portGabrielWong <gw@port.finance>
2022-04-28 13:30:59 +0800

    Added set lending market owner instructions.

---
+++ src/instructions/lending/setLendingMarketOwner.ts
@@ -0,0 +1,45 @@
+import { PublicKey, TransactionInstruction } from "@solana/web3.js";
+import { Buffer } from "buffer";
+import * as BufferLayout from "@solana/buffer-layout";
+import { PORT_LENDING } from "../../constants";
+import { AccessType, getAccess } from "../../utils/Instructions";
+import * as Layout from "../../serialization/layout";
+import { LendingInstruction } from "./instruction";
+
+const DataLayout = BufferLayout.struct([
+  BufferLayout.u8("instruction"),
+  Layout.publicKey("newOwner"),
+]);
+
+// Update the owner of the lending Market.
+//
+// Accounts expected by this instruction:
+//
+//   0. `[writable]` Lending market account.
+//   1. `[signer]` Previous owner.
+export const initLendingMarketInstruction = (
+  lendingMarket: PublicKey,
+  lendingMarketOwner: PublicKey,
+  newOwner: PublicKey,
+  lendingProgramId: PublicKey = PORT_LENDING
+): TransactionInstruction => {
+  const data = Buffer.alloc(DataLayout.span);
+  DataLayout.encode(
+    {
+      instruction: LendingInstruction.SetLendingMarketOwner,
+      newOwner,
+    },
+    data
+  );
+
+  const keys = [
+    getAccess(lendingMarket, AccessType.WRITE),
+    getAccess(lendingMarketOwner, AccessType.READ),
+  ];
+
+  return new TransactionInstruction({
+    keys,
+    programId: lendingProgramId,
+    data,
+  });
+};
